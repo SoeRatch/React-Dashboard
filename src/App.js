b7d3fe5aceb245769a31d502339da339e6fb4aac
SoeRatch <surajkoho@gmail.com>
2019-02-05 18:11:51 +0530

    project structure initialized with react in src folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+import { hot } from 'react-hot-loader/root'
+
+const App = ({location}) => (
+			    <div>Hello World</div>
+
+);
+
+
+export default hot(App)
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import App from './App';
+
+
+ReactDOM.render(
+	<BrowserRouter>
+		<Route component={App}/>
+	</BrowserRouter>,
+	document.getElementById('app')
+);
+
