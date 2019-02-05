b86e2fe7c1f8aeb439bd6ce181f7b77718d54d43
SoeRatch <surajkoho@gmail.com>
2019-02-05 18:21:20 +0530

    project structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from "react-router-dom";
 import { hot } from 'react-hot-loader/root'
 
+
 const App = ({location}) => (
 			    <div>Hello World</div>
 
